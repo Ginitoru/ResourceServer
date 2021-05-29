1b1c89da2e27e0ee454a316b1aa502c67bb7b267
iordache <faraonu_ginitoru@yahoo.com>
2021-05-29 21:48:26 +0300

    initiating ResourceServerApplication. Creating web module. Added links to the database and dependencies

---
+++ web/src/main/java/com/iordache/web/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.iordache.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+}
