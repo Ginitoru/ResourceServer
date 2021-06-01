01f05c08b61af83c7d4127cfbaa30ac6ce2db5d5
iordache <faraonu_ginitoru@yahoo.com>
2021-06-01 17:47:47 +0300

    added @EntityScan to SpringBoot aplication to scan the entity classes and automaticly creating the tables. Added domain module as a dependency to web module so that the application knows where to scan for the entities

--- web/src/main/java/com/iordache/web/ResourceServerApplication.java
+++ web/src/main/java/com/iordache/web/ResourceServerApplication.java
@@ -3,16 +3,20 @@ package com.iordache.web;
 import com.iordache.security.ResourceServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan({"com.iordache.web.*"})
 @Import({ResourceServerConfiguration.class})
+@EntityScan({"com.iordache.domain.entity"})
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceServerApplication.class, args);
     }
+
+
 
 }
